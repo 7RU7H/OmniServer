b16102f83b56046277c10e524994a4cc82e23399
kali <mkthun@protonmail.com>
2024-02-05 13:29:39 +0000

    Err handling shaffolded, Deadending switch 2 test

--- main.go
+++ main.go
@@ -17,16 +17,15 @@ import (
 // Either: http or https server
 
 // TODO List TODO
-// Refactor out structs use a sorted array
-// Best CheckError function for all the golangs
+// BUILD AND BUILD
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
 // HTTP server
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
 // go routine and channels
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
-// TLS
+// TLS - regex requred that make sense, validationTLS(), how validateTLS passes data to buildHTTPS()
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
-// HTTPS server
+// runHTTPSserver()
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
 // Add all the profession stuff
 // - DONOT WORRY ABOUT nested regex -> string sorted args oneliners no (5||6)*2 additional variable declarations making that underreadable dense vertically and save some memory
@@ -42,21 +41,21 @@ func handleArgs(args []string) ([]string, error) {
 	sortedArgs := make([]string, len(args)+1)
 
 	matchIP, err := regexp.MatchString(ipRegex.String(), regexSafeArgs)
-	checkError(err)
+	checkError(err, 0)
 	matchPort, err := regexp.MatchString(portRegex.String(), regexSafeArgs)
-	checkError(err)
+	checkError(err, 0)
 
 	if len(args) != 6 {
 		matchHTTP, err := regexp.MatchString(httpRegex.String(), regexSafeArgs)
-		checkError(err)
+		checkError(err, 0)
 		tmpReduced := httpRegex.ReplaceAllString(regexSafeArgs, "")
 		tmpReduced = ipRegex.ReplaceAllString(tmpReduced, "")
 		tmpReduced = portRegex.ReplaceAllString(tmpReduced, "")
 		ifconfigPotentialName := strings.ReplaceAll(tmpReduced, "#", "")
 		ifconfig, err := net.InterfaceByName(ifconfigPotentialName)
-		checkError(err)
+		checkError(err, 0)
 		ifconfigCIDRTmp, err := convIfconfigNameToCIDR(ifconfig)
-		checkError(err)
+		checkError(err, 0)
 		httpAllMatched := matchHTTP && matchIP && matchPort
 		if !httpAllMatched {
 			err := fmt.Errorf("Arguments provided are %v: %v", httpAllMatched, args)
@@ -72,23 +71,23 @@ func handleArgs(args []string) ([]string, error) {
 			err := fmt.Errorf("Invalid IP and Port combination: %s and %s", sortedArgs[3], sortedArgs[4])
 			return nil, err
 		}
-		checkError(err)
+		checkError(err, 0)
 		return sortedArgs, nil
 
 	} else {
 		matchHTTPS, err := regexp.MatchString(httpsRegex.String(), regexSafeArgs)
-		checkError(err)
+		checkError(err, 0)
 		tmpReduced := httpRegex.ReplaceAllString(regexSafeArgs, "")
 		tmpReduced = ipRegex.ReplaceAllString(tmpReduced, "")
 		tmpReduced = portRegex.ReplaceAllString(tmpReduced, "")
 		tmpReduced = tlsRegex.ReplaceAllString(tmpReduced, "")
 		ifconfigPotentialName := strings.ReplaceAll(tmpReduced, "#", "")
 		ifconfig, err := net.InterfaceByName(ifconfigPotentialName)
-		checkError(err)
+		checkError(err, 0)
 		ifconfigCIDRTmp, err := convIfconfigNameToCIDR(ifconfig)
-		checkError(err)
+		checkError(err, 0)
 		matchTLS, err := regexp.MatchString(tlsRegex.String(), regexSafeArgs)
-		checkError(err)
+		checkError(err, 0)
 		httpsAllMatched := matchHTTPS && matchIP && matchPort && matchTLS
 		if !httpsAllMatched {
 			err := fmt.Errorf("Arguments provided are %v: %v", httpsAllMatched, args)
@@ -104,7 +103,7 @@ func handleArgs(args []string) ([]string, error) {
 			err := fmt.Errorf("Invalid IP and Port combination: %s and %s", sortedArgs[3], sortedArgs[4])
 			return nil, err
 		}
-		checkError(err)
+		checkError(err, 0)
 		return sortedArgs, nil
 	}
 }
@@ -122,21 +121,67 @@ func main() {
 	args, argsLen := flag.Args(), len(flag.Args())
 
 	if argsLen > 4 {
-		// Requires
+		err := fmt.Errorf("The number arguments provided was %d", argsLen)
+		checkError(err, 0)
 		os.Exit(1)
 	}
 
 	sortedArgs, err := handleArgs(args)
-	checkError(err)
+	checkError(err, 0)
 
 	// len +1 includes hostname checks
 	switch sortedArgs[0] {
 	case "http":
+		err := buildHTTPServer(sortedArgs)
+		checkError(err, 0)
+		err = runHTTPServer()
+		checkError(err, 0)
+		err = gracefulExit()
+		checkError(err, 0)
+		break
 	case "https":
-		// Validate TLS
-	default:
-	}
+		tlsReq, err := validateTLS(sortedArgs[5])
+		checkError(err, 0)
+		err = buildHTTPSServer(sortedArgs[0:4], tlsReq)
+		checkError(err, 0)
+		err = runHTTPSServer()
+		checkError(err, 0)
+		err = gracefulExit()
+		checkError(err, 0)
+		break
 
+	default:
+		err := fmt.Errorf("Invalid Sorted arguments and proof at index 0: %s", sortedArgs[0])
+		checkError(err, 0)
+	}
+	return
+}
+
+func buildHTTPServer(args []string) error {
+	fmt.Println("building HTTP")
+	return nil
+}
+func runHTTPServer() error {
+	fmt.Println("running HTTP")
+	return nil
+}
+func validateTLS(args string) (string, error) {
+	fmt.Println("validating TLS")
+	return "TLS incoming", nil
+}
+
+func buildHTTPSServer(nontlsArgs []string, tls string) error {
+	fmt.Println("building HTTPS")
+	return nil
+}
+func runHTTPSServer() error {
+	fmt.Println("running HTTPS")
+	return nil
+}
+
+func gracefulExit() error {
+	fmt.Println("SIG THIS or something fanciy please!")
+	return nil
 }
 
 func prependColonToPortNumber(port string) string {
@@ -147,16 +192,17 @@ func prependColonToPortNumber(port string) string {
 	return listeningPort
 }
 
-// Build and improve
-func checkError(err error) {
+// Everything is fatal till EVERYTHING WORKS relax those switch statement fingers custom error switch
+// Use regex `(err, \d)` to find and change incrementally later - the error code make life easier after all ALWAYS err.New or err.Errorf
+func checkError(err error, errorCode int) {
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err, "Error code: %d", errorCode)
 	}
 }
 
 func checkFileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
-	if err == nil {
+	if err != nil {
 		log.Fatal(err)
 		return false, err
 	}
@@ -169,7 +215,7 @@ func checkFileExists(path string) (bool, error) {
 
 func checkValidPort(portStr string) bool {
 	portInt, err := strconv.Atoi(strings.ReplaceAll(portStr, ":", ""))
-	checkError(err)
+	checkError(err, 0)
 	if portInt <= 65535 && portInt > -1 {
 		return true
 	}
@@ -196,7 +242,7 @@ func checkValidIP(ip string) bool {
 
 func convIfconfigNameToCIDR(ifconfig *net.Interface) (string, error) {
 	addrs, err := ifconfig.Addrs()
-	checkError(err)
+	checkError(err, 0)
 	for _, addr := range addrs {
 		if ipNet, ok := addr.(*net.IPNet); ok {
 			return ipNet.String(), nil
