57302f1649e4cea7e1c97e7454ac6dcc8018e90d
kali <mkthun@protonmail.com>
2024-02-05 20:14:46 +0000

    Minor fixes and figuring out path to making httpserver testable

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // CLI -> if http else https -> Done - just simple done project - below is just a map of functions - see TODO idiot
@@ -17,9 +21,10 @@ import (
 // Either: http or https server
 
 // TODO List TODO
-// Check if binary is Called OmniServer - well is one way to make this just for CTFs I suppose
 // HTTP server
+// Error ids and code - id for where in the source for no lost in the src and code for switch case fatal or not
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
+// Application end - start time printTotalRuntime()
 // go routine and channels
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
 // TLS - regex requred that make sense, validationTLS(), how validateTLS passes data to buildHTTPS()
@@ -27,148 +32,143 @@ import (
 // runHTTPSserver()
 // TODO AFTER THE ABOVE IS COMPLETE built and works no excuses:
 // Add all the profession stuff
+// - Make authentication actually work without any other packages
+// - Colourful Banner!
 // - DONOT WORRY ABOUT nested regex -> string sorted args oneliners no (5||6)*2 additional variable declarations making that underreadable dense vertically and save some memory
 
 // Download file - filename
 func DownloadFileHandler(w http.ResponseWriter, r *http.Request) error {
-        // client := Headers - IP User-Agent
-        
-	requestedFile := 
-        if checkFileExists(requestedFile) {
-                
+	// client := Headers - IP User-Agent
 
-
-
-		w.WriteHeader(404)
-                w.Write([]byte("404\n"))
-		// Use logging table template and then Fatal
-                log.Fatal("Failed to Download file: %s - requested by: %s, %s", requestedFile, clientIP, clientUA)
-                // Fail to download file error
-                return err
-        } else {
-                startTime := time.Now()
-                log.Printf("Downloading file at:  %s - requested by: %s, %s", requestedFile, clientIP, clientUA)
-        }
-        endTime := time.Now()
-        log.Printf("Successfully Download File - %s by %s\n", filename, clientIP, clientUA)
-        return nil
+	requestedURL := ""
+	requestedFile := ""
+	clientIP := ""
+	clientMAC := ""
+	clientUA := ""
+	exists, err := checkFileExists(requestedFile)
+	if !exists {
+		http.NotFound(w, r)
+		log.Println("Failed to Download file: %v from %v - requested by: %v, %v, %v", requestedFile, requestedURL, clientIP, clientMAC, clientUA)
+		return err
+	} else {
+		startTime := time.Now()
+		http.ServeFile(w, r, requestedFile)
+		log.Printf("Downloading file at: %v from: %v - requested by: %v, %v, %v\n", requestedFile, requestedURL, clientIP, clientMAC, clientUA)
+	}
+	endTime := time.Now()
+	log.Printf("Successfully Downloaded File - %v by %v - %v\n", requestedFile, clientIP, clientMAC)
+	return nil
 }
 
-
-
-
-// Upload file - filename
+// TODO
 func UploadFileHandler(w http.ResponseWriter, r *http.Request) error {
-	
-        // Parse our multipart form, 10 << 20 specifies a maximum
-        // upload of 10 MB files.
-        //r.ParseMultipartForm(10 << 20)
 
-        // Get filename from body of r.Body
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	//r.ParseMultipartForm(10 << 20)
 
-        // FormFile returns the first file for the given key `myFile`
-        // it also returns the FileHeader so we can get the Filename,
-        // the Header and the size of the file
+	// Get filename from body of r.Body
 
-        log.Printf("/upload/%s - Upload requested by ...", filename)
-        file, handler, err := r.FormFile()
-        if err != nil {
-                // Error retrieving file of filename
-                return err
-        }
-        startTime := time.Now()
-        defer file.Close()
-        //log.Print("",  ) File upload request success
-        //log.Print("",  ) File upload INFO:
-        log.Printf("Uploaded File: %+v\n", filename)
-        lof.Printf("File Size: %+v\n", fileSize)
-        log.Printf("MIME Header: %+v\n", mimeHeader)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
 
-        //log.Print("",  ) File upload request success
+	log.Printf("/upload/%s - Upload requested by ...", filename)
+	file, handler, err := r.FormFile()
+	if err != nil {
+		// Error retrieving file of filename
+		return err
+	}
+	startTime := time.Now()
+	defer file.Close()
+	//log.Print("",  ) File upload request success
+	//log.Print("",  ) File upload INFO:
+	log.Printf("Uploaded File: %+v\n", filename)
+	lof.Printf("File Size: %+v\n", fileSize)
+	log.Printf("MIME Header: %+v\n", mimeHeader)
 
-        // Create a temporary file within our temp-images directory that conforms to a naming scheme
-        tempFile, err := os.TempFile(tmpUploadDir, "tmp-")
-        if err != nil {
-                // Error creating temporary file
-                return err
-        }
-        defer tempFile.Close()
+	//log.Print("",  ) File upload request success
 
-        // read all of the contents of our uploaded file into a byte array
-        fileBytes, err := os.ReadFile(file)
-        if err != nil {
-                // Failed to read file being uploaded to byte array
-                return err
-        }
-        // write this byte array to our temporary file
-        tempFile.Write(fileBytes)
-        endTime := time.Now()
-        //Return that we have successfully uploaded our file!
-        log.Printf("Successfully Uploaded File - %s \n", filename)
-        fmt.Fprintf(w, "Successfully Uploaded File - %s \n", filename)
-        return nil
+	// Create a temporary file within our temp-images directory that conforms to a naming scheme
+	tempFile, err := os.TempFile(tmpUploadDir, "tmp-")
+	if err != nil {
+		// Error creating temporary file
+		return err
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a byte array
+	fileBytes, err := os.ReadFile(file)
+	if err != nil {
+		// Failed to read file being uploaded to byte array
+		return err
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	endTime := time.Now()
+	//Return that we have successfully uploaded our file!
+	log.Printf("Successfully Uploaded File - %s \n", filename)
+	fmt.Fprintf(w, "Successfully Uploaded File - %s \n", filename)
+	return nil
 }
-
-
 
 func saveReqBodyFileHandler(r *http.Request) error {
-        builder := strings.Builder()
-        startTime := time.Now()
-        builder.WriteString(os.TempDir())
-        builder.WriteString("/")
-        builder.WriteString(strings.ReplaceAll(r.RemoteAddr, ".", "-")
+	builder := strings.Builder()
+	startTime := time.Now()
+	builder.WriteString(os.TempDir())
+	builder.WriteString("/")
+	builder.WriteString(strings.ReplaceAll(r.RemoteAddr, ".", "-"))
 	builder.WriteString("-T-")
-        builder.WriteString(strconv.Itoa(int(time.Now().Unix())))
-        filepath := builder.String()
-        err := os.Create(filepath,  0644)
-        if err != nil {
-                log.Fatal(err)
-                return err
-        }
-        err := io.Copy(filepath, r.Body)
-        if err != nil {
-                log.Fatal(err)
-                return err
-        }
-        defer f.Close()
+	builder.WriteString(strconv.Itoa(int(time.Now().Unix())))
+	filepath := builder.String()
+	err := os.Create(filepath, 0644)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	err := io.Copy(filepath, r.Body)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer f.Close()
 	builder.Flush()
-        endTime := time.Now()
-	log.Println("Entire process of file creation for file upload: %v - took from: %v till %v",filepath, startTime, endTime)
-        return nil
+	endTime := time.Now()
+	log.Println("Entire process of file creation for file upload: %v - took from: %v till %v", filepath, startTime, endTime)
+	return nil
 }
 
 func createDefaultWebServerMux() *ServerMux {
-        mux := http.NewServeMux()
-        mux.HandleFunc("/upload", uploadFileHandler())
-        mux.HandleFunc("/download", downloadFileHandler())
-        mux.HandleFunc("/saveReqBody", saveReqBodyFileHandler())
-        return mux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/upload", uploadFileHandler())
+	mux.HandleFunc("/download", downloadFileHandler())
+	mux.HandleFunc("/saveReqBody", saveReqBodyFileHandler())
+	return mux
 }
 
-func initServerContext(lportString, keyServerAddr string, srvMux *ServerMux) (*http.Server, Context, CancelFunc, error) {
-        ctx, cancelCtx := context.WithCancel(context.Background())
-        server := &http.Server{
-                Addr:    lportString,
-                Handler: srvMux,
-                BaseContext: func(l net.Listener) context.Context {
-                        ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-                        return ctx
-                },
-        }
-        return server, ctx, cancelCtx, nil
+func initServerContext(lportString, keyServerAddr string, srvMux *http.ServerMux) (*http.Server, context.Context, context.CancelFunc, error) {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	server := &http.Server{
+		Addr:    lportString,
+		Handler: srvMux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+	return server, ctx, cancelCtx, nil
 }
 
-
-func buildHTTPServer(args []string) (*http.Server, Context, CancelFunc, error) {
-	log.Println("Building HTTP")
+func buildHTTPServer(args []string) (*http.Server, context.Context, context.CancelFunc, error) {
+	log.Println("--- Building HTTP Server ---")
 	mux := createDefaultWebServerMux()
-	log.Println("Mux for %v created", args) 
- 	httpServer, ctx, cancelCtx, err := initServerContext(args[4], ,mux)	
-	checkError(err)
-	log.Println("Server Built for %v created", httpServer) 
+	log.Println("Mux for %v created", args)
+	httpServer, ctx, cancelCtx, err := initServerContext(args[4], args[3], mux)
+	checkError(err, 0)
+	log.Println("--- Server Built for %v created ---", httpServer)
 	return httpServer, ctx, cancelCtx, nil
 }
-func runHTTPServer() error {
+func runHTTPServer(*http.Server, context.Context, context.CancelFunc) error {
 	fmt.Println("running HTTP")
 	return nil
 }
@@ -372,6 +372,12 @@ func handleArgs(args []string) ([]string, error) {
 		return sortedArgs, nil
 	}
 }
+func printTotalRuntime(appStartTime time.Time) {
+	appTerminateTime := time.Now()
+	totalRuntime := 0
+	// Do the mathematics idiot
+	log.Printf("Application started: %v - Terminated: %v - Runtime: %v\n", appStartTime, appTerminateTime, totalRuntime)
+}
 
 func main() {
 	var ipAddress, serverType, tlsInputStr, netInterface string
@@ -382,7 +388,9 @@ func main() {
 	flag.StringVar(&ipAddress, "i", "127.0.0.1", "Provide a valid IPv4 address - required!")
 	flag.IntVar(&portInt, "p", 8443, "Provide a TCP port number - required!")
 	flag.Parse()
+	// Banner !!
 
+	appStartTime := time.Now()
 	args, argsLen := os.Args, len(os.Args)
 
 	if argsLen > 9 {
@@ -398,9 +406,9 @@ func main() {
 
 	switch sortedArgs[0] {
 	case "http":
-		err := buildHTTPServer(sortedArgs)
+		httpServer, ctx, cancelCtx, err := buildHTTPServer(sortedArgs)
 		checkError(err, 0)
-		err = runHTTPServer()
+		err = runHTTPServer(httpServer, ctx, cancelCtx)
 		checkError(err, 0)
 		err = gracefulExit()
 		checkError(err, 0)
@@ -420,5 +428,7 @@ func main() {
 		err := fmt.Errorf("Invalid Sorted arguments and proof at index 0: %s", sortedArgs[0])
 		checkError(err, 0)
 	}
+
+	printTotalRuntime(appStartTime)
 	return
 }
